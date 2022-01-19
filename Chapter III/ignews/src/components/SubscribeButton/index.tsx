959242b8e6ae3a38d99da8e5f674d7cc7e97d12f
Taiuan Pagini <taiuan.pagini@apinteractive.com.br>
2022-01-19 11:00:42 -0300

    projeto ignews completo

--- Chapter III/ignews/src/components/SubscribeButton/index.tsx
+++ Chapter III/ignews/src/components/SubscribeButton/index.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import { session, signIn } from 'next-auth/client';
+import { signIn, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
+import { useRouter } from 'next/router';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export const SubscribeButton: React.FC<SubscribeButtonProps> = ({ priceId }) => {
+  const [session] = useSession();
+  const router = useRouter();
 
   const handleSubscribe = async () => {
     if (!session) {
       signIn('github');
+
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts');
 
       return;
     }
